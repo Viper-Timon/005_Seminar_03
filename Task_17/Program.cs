6809325c092ffe232c93bbd0b7aa69168adc7483
viper-timon <viper-timon@mail.ru>
2022-05-16 20:49:46 +0300

    task 17 18 done

---
+++ Task_17/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите координату X:");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y:");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0) Console.WriteLine("1");
+else if (x > 0 && y > 0) Console.WriteLine("Диапазон 1");
+else if (x < 0 && y > 0) Console.WriteLine("Диапазон 2");
+else if (x < 0 && y < 0) Console.WriteLine("Диапазон 3");
+else if (x > 0 && y < 0) Console.WriteLine("Диапазон 4");
+else Console.WriteLine ("Введите координаты отличные от 0");
---
+++ Task_18/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Console.WriteLine("Введите координату X:");
+// int x = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите координату Y:");
+// int y = int.Parse(Console.ReadLine());
+// if (x > 0 && y > 0) Console.WriteLine("1");
+// else if (x > 0 && y > 0) Console.WriteLine("Диапазон 1");
+// else if (x < 0 && y > 0) Console.WriteLine("Диапазон 2");
+// else if (x < 0 && y < 0) Console.WriteLine("Диапазон 3");
+// else if (x > 0 && y < 0) Console.WriteLine("Диапазон 4");
+// else Console.WriteLine ("Введите координаты отличные от 0");
+
+
+Console.WriteLine("Введите номер диапазона от 1 до 4");
+int diap = int.Parse(Console.ReadLine());
+if (diap == 1) Console.WriteLine("Соотетствует значениям x > 0 и y > 0");
+else if (diap == 2) Console.WriteLine("Соотетствует значениям x < 0 и y > 0");
+else if (diap == 3) Console.WriteLine("Соотетствует значениям x < 0 и y < 0");
+else if (diap == 4) Console.WriteLine("Соотетствует значениям x > 0 и  < 0");
+else Console.WriteLine("Вы ввели неверный номер диапазона");
