949a05173bd538255e02e5b7b9d1b31f627fb269
viper-timon <viper-timon@mail.ru>
2022-05-23 00:42:09 +0300

    tasks 19, 21(#2), 23 done

---
+++ Task_19_HW/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 19: Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, является ли оно
+// палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+// Решение через метод с циклом
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+void FindNumPalin(int num)
+{
+    // определение количества знаков 
+    if (num < 0) num = num * -1;
+    int count = 0;
+    int num1 = num;
+    int num2 = num;
+    
+    while (num1 > 0)
+    {
+        num1 = num1 / 10;
+        count++;
+    }
+
+    // создание и заполнение массива
+    int[] col1 = new int[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        col1[i] = num2 % 10;
+        num2 = num2 / 10;
+    }
+
+    int collen = col1.Length;
+    string result = "Не вышло"; 
+
+    // цикл для сравнения
+    for (int j = 0; j < (collen / 2); j++)
+    {
+       if (col1[j] == col1[(collen-1)-j])
+       {   
+            result = "Да";
+       }
+       else
+       {
+            result = "Нет";
+            break;
+       }
+    }
+    Console.WriteLine(result);
+    
+}
+
+FindNumPalin(number);
---
+++ Task_21_HW/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 21: Напишите программу, которая принимает на
+// вход координаты двух точек и находит расстояние между
+// ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Clear();
+Console.Write("Введите X1 -> ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y1 -> ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z1 -> ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"A ({x1}, {y1}, {z1})");
+Console.Write("Введите X2 -> ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y2 -> ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z2 -> ");
+int z2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"B ({x2}, {y2}, {z2})");
+//
+double result = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine(Math.Round(result, 3));
